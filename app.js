d759b47a8bc0c9aa35a0bbfd6640a66d60839d65
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-11-08 10:54:25 +0900

    rearrange the req.on('end') event listener to not block the code
    I'm applying the event drive architecture to allow node to create
    threads for each event and keep the event loop runing. It allows Node
    to create threads of event that can run later and keep the server
    open for new requests
    
    I changed the fs.writeFileSync() to fs.writeFile() to write the file
    async and provide a third argument, an arrow function to run when the
    file is written. It allows the server to keep in the event loop and
    accepting new requests, which is ideal

--- app.js
+++ app.js
@@ -4,76 +4,83 @@
 //the http module comes with Node.js, but not exposed globally
 
 //importing http module
-const http = require('http');
+const http = require("http");
 
 //import the file manager module
-const fs = require('fs');
+const fs = require("fs");
 
 // function rqListener(req, res) {
 // // this function is created to be dealed with request and response, in this order.
 
 // }
 
-//the create Server function requires a function nomination as argument. 
+//the create Server function requires a function nomination as argument.
 //everytime is gets a request, it will call the function in the argument to deal with those requests
 const server = http.createServer((req, res) => {
-//you can use anonimous function or arrow function in the createServer();
- //this creates a event listener that keep the server in the Event loop
+  //you can use anonimous function or arrow function in the createServer();
+  //this creates a event listener that keep the server in the Event loop
 
-const url = req.url;
-const method = req.method;
-if (url === '/') {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
     //check if the url from the http req is the root '/'
     res.write("<html>");
     res.write("<head><title>Enter message</title></head>");
-    res.write("<body><form action='/message' method='POST'><input name='message' type='text'><button type='submit'>Send</button></form></body>");
+    res.write(
+      "<body><form action='/message' method='POST'><input name='message' type='text'><button type='submit'>Send</button></form></body>"
+    );
     res.write("</html>");
     return res.end();
-}
+  }
 
-if (url === '/message' && method === 'POST') {
+  if (url === "/message" && method === "POST") {
     //check is the url from the http req is the '/message' and the method is POST
 
     const body = [];
 
     //creating an event listener to everytime there is data available to read on the buffer
-    req.on('data', (chunk) => {
-        console.log(chunk);
-        body.push(chunk);
-    })
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
 
     //creating an event listener to the end of the data stream
-    req.on('end', () => {
-        //using the buffer global object available by Node.js
-        const parsedBody = Buffer.concat(body).toString();
+    return req.on("end", () => {
+      //using the buffer global object available by Node.js
+      const parsedBody = Buffer.concat(body).toString();
 
-        //output: message=Hello+Server
-        console.log(parsedBody);
+      //output: message=Hello+Server
+      console.log(parsedBody);
 
-        //storing the message data in a const
-        const message = parsedBody.split('=')[1];
+      //storing the message data in a const
+      const message = parsedBody.split("=")[1];
 
-        //creating a file upon http request in which is written DUMMY
-        //this has to be inside the req.on('end') because this code runs at the end. 
-        fs.writeFileSync('message.txt', message);
-        //file written: Hello+Server
-    })
+      //creating a file upon http request in which is written DUMMY
+      //this has to be inside the req.on('end') because this code runs at the end.
+      fs.writeFile("message.txt", message, (error) => {
+        //the third argument in the writeFile() function is is a function that runs after the file is done
+        //this function receives an error object which is null if there was no error.
 
-    //setting the status Code to 302 that means redirected
-    res.statusCode = 302;
+        //setting the status Code to 302 that means redirected
+        res.statusCode = 302;
 
-    //redirecting to localhost:3000/
-    res.setHeader('Location', '/');
-    return res.end();
-    
-}
+        //redirecting to localhost:3000/
+        res.setHeader("Location", "/");
+        return res.end();
+      });
 
- res.setHeader('Content-Type', 'text/html');
- res.write("<html>");
- res.write("<head><title>My first server response</title></head>");
- res.write("<body><h1>Hello from my Node.js server</h1></body>");
- res.write("</html>");
- res.end();
+      //fs.writeFileSync('message.txt', message);
+      //the Sync function blocks the code until the file is created.
+      //file written: Hello+Server
+    });
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first server response</title></head>");
+  res.write("<body><h1>Hello from my Node.js server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
