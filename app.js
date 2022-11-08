1c3ea364784ae2263bbe953f33768e1ec24cd7be
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-11-09 08:33:10 +0900

    create two middlewares with express to control req flow
    express allow to app.use() middlewares.
    the app.use() method receives three arguments, the req, the res and the
    third argument is next, with is a function that lets the http request
    travel to next middleware in line

--- app.js
+++ app.js
@@ -7,7 +7,20 @@ const express = require('express');
 //create an app using express()
 const app = express();
 
+//express creates a set of middleware function that deal with the request/response logic
+//after declaring the app and before passing to the server, you can use express to add more
+//middleware functions
 
+app.use((req, res, next) => {
+    console.log("in the middleware");
+
+    //next() must be called to let the request to travel to the next middleware in line
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("in the second middleware");
+});
 
 
 //add the app to the createServer() without the parenthesys.
