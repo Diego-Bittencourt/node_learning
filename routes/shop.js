a799ef4118aecc96c3e25e8c3b67d9d3a7b15123
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-11-12 23:24:19 +0900

    Output dynamic data using pug special syntax #{}
    When setting a res.render(), you set the file to use as template and,
    as second argument, you can pass an object with data to be used in the
    template
    
    inside the template, you can show the data received from the
    res.render() method by using the syntax #{} that, inside the {}, you
    put the data. Keep in mind that all data comes inside one object, so
    use dot notation.
    
    In this case, I used prods.title in which prods is the key value I pick
    when passing data with the res.render() method and the title is the
    inside the object received from the shop.js file.

--- routes/shop.js
+++ routes/shop.js
@@ -9,8 +9,11 @@ const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
+    console.log(adminData.products)
+    const products = adminData.products;
     //the render method uses the template engine to run a template
-   res.render('shop');
+    //you can pass data to the render method by adding a second argument with an object and a key name at your choice
+   res.render('shop', { prods: products, docTitle: "Shop" });
    //It's not necessary to tell the path because the template engine tells the path and
    //also, don't need to add the extension because this is the default, stated in the app.js file in the app.set() method
 });
