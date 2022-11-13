af380acf89dab5fb37db952617a7b4a3a2c336de
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-11-13 12:13:28 +0900

    create the pug file for the add-product page and the 404 page

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(shopRoutes);
 
 //catch all requests with invalid url
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render("404");
 })
 
 //add the app to the createServer() without the parenthesys.
--- routes/admin.js
+++ routes/admin.js
@@ -19,18 +19,13 @@ const products = [];
 //the /admin segment of the url was already filtered in the app.js file and don't use it here.
 //filtering for /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    //sending the form
-    // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
-    //'<form action="/admin/add-product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
-
-    //using the rootDir variable I created in the util folder
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    res.render('add-product', { pageTitle: 'Add Product' });
 });
 
 //filtering for /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
     //the req.body is a new field creted by express.js
-    products.push({title: req.body.title})
+    products.push({ title: req.body.title })
     res.redirect('/');
 })
 
