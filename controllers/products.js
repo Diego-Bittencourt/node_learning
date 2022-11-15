36488c73c284334b3a94145fdc6c6def8552f6fe
diego-bittencourt <mendesdiego90@gmail.com>
2022-11-15 13:54:18 +0900

    Create a products.json file to write and read data from

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,11 @@
-const Product = require('../models.product');
+const Product = require('../models/product');
 
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
         pageTitle: 'Add Product', 
-        pageTitle: 'Add Product', 
         path: '/admin/add-product', 
-        ctiveAddProduct: true, 
+        activeAddProduct: true, 
         formsCSS: true, 
         productCSS: true
     });
@@ -20,10 +19,27 @@ exports.postAddProduct = (req, res, next) => {
 
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
+
+    //to fix the issue described in the fetchAll() function, I'll pass a callback function in
+    //the function call
+    //it's the same approach in the readFile(), although I had to write both sides here
+    Product.fetchAll((products) => {
+        //fetchAll is going to run this arrow function when it's done.
+        res.render('shop', { 
+            prods: products, 
+            pageTitle: 'Shop', 
+            path: '/', 
+            hasProducts: products.length > 0, 
+            activeShop: true, 
+            productCSS: true
+        });
+    });
+    //I didn't call the function in a variale because it doesn't return anything.
+
+
     //the render method uses the template engine to run a template
     //you can pass data to the render method by adding a second argument with an object and a key name at your choice
-    res.render('shop', { prods: products, pageTitle: "Shop", path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true });
+    
     //It's not necessary to tell the path because the template engine tells the path and
     //also, don't need to add the extension because this is the default, stated in the app.js file in the app.set() method
 };
