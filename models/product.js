bec8f3b2d9a8a6848f1a0f72e6a1a26a26dc11ca
diego-bittencourt <mendesdiego90@gmail.com>
2022-11-15 16:49:31 +0900

    add form to hold price, description and other fields

--- models/product.js
+++ models/product.js
@@ -31,8 +31,11 @@ const localpath = require('../util/path');
 
 module.exports = class Product {
 
-    constructor(t) {
-        this.title = t;
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
